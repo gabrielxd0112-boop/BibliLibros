1b821c1f0a8931fc15a37e01811982af3077c006
alex <alex@PKONG>
2025-11-10 00:00:32 -0500

    Agregar archivos de proyecto.

---
+++ Clases/ListaDoble.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class ListaDoble
+    {
+        public NodoDoble inicio, fin;
+
+        public void Agregar(string titulo, string autor)
+        {
+            NodoDoble nuevo = new NodoDoble();
+            nuevo.titulo = titulo;
+            nuevo.autor = autor;
+
+            if (inicio == null)
+                inicio = fin = nuevo;
+            else
+            {
+                fin.siguiente = nuevo;
+                nuevo.anterior = fin;
+                fin = nuevo;
+            }
+        }
+
+        public void MostrarAdelante()
+        {
+            NodoDoble temp = inicio;
+            while (temp != null)
+            {
+                Console.WriteLine($"{temp.titulo} - {temp.autor}");
+                temp = temp.siguiente;
+            }
+        }
+    }
+}
---
+++ Clases/ListaSimple.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class ListaSimple
+    {
+        public NodoSimple inicio;
+
+        public void Agregar(string nombre)
+        {
+            NodoSimple nuevo = new NodoSimple();
+            nuevo.nombre = nombre;
+
+            if (inicio == null)
+                inicio = nuevo;
+            else
+            {
+                NodoSimple temp = inicio;
+                while (temp.siguiente != null)
+                    temp = temp.siguiente;
+                temp.siguiente = nuevo;
+            }
+        }
+
+        public void Mostrar()
+        {
+            NodoSimple temp = inicio;
+            while (temp != null)
+            {
+                Console.WriteLine("- " + temp.nombre);
+                temp = temp.siguiente;
+            }
+        }
+    }
+}
